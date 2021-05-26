5a77380e88aedf858403d4c5613bb7de6574e547
unknown <carlos.viniciusufrpe@gmail.com>
2021-05-26 11:56:30 -0300

    Cortando canos sob nova administração

--- Colheita de caju/Colheita de caju.py
+++ Colheita de caju/Colheita de caju.py
@@ -1,4 +1,4 @@
-l,c,m,n = [int(numero) for numero in input().split()]
+6l,c,m,n = [int(numero) for numero in input().split()]
 fazenda = []
 for j in range(l):
     caju = [int(numero) for numero in input().split()]
---
+++ Cortando canos sob nova administração/Cortando canos sob nova administração.py
@@ -0,0 +1,49 @@
+def mochila(i,j,comprimentoValor):
+    if i == 0 or j == 0:
+        return 0
+    else:
+        t = []
+
+        for linha in range(i+1):
+            t.append([])
+            for coluna in range(j + 1):
+                if linha == 0 or coluna == 0:
+                    t[linha].append(0)
+                else:
+                    if comprimentoValor[linha-1][0] > coluna:
+                        t[linha].append(t[linha-1][coluna])
+                    else:
+                        resultado = defineMaximo(t[linha-1][coluna], t[linha-1][coluna-comprimentoValor[linha-1][0]] + comprimentoValor[linha-1][1])
+
+                        if resultado > 0 and resultado > t[linha-1][coluna]:
+                            t[linha].append(0)
+                            corte_de_cano = coluna
+                            while corte_de_cano >= comprimentoValor[linha-1][0]:
+                                corte_de_cano -= comprimentoValor[linha-1][0]
+                                t[linha][coluna] += comprimentoValor[linha-1][1]
+
+                        else:
+                            t[linha].append(resultado)
+
+    return t[i][j]
+
+def defineMaximo(termoA, termoB):
+
+    if termoA > termoB:
+        return termoA
+    else:
+        return termoB
+
+
+
+
+numero, tamanhoMaximo = [int(e) for e in input().split()]
+comprimentoValor = []
+for e in range(numero):
+    pedido = tuple(map(int, input().split()))
+    comprimentoValor.append(pedido)
+
+print(mochila(numero,tamanhoMaximo,comprimentoValor))
+
+
+
