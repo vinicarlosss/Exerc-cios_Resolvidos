30ef9dcec71cd65fbbb750da19ae044dec4c3601
unknown <carlos.viniciusufrpe@gmail.com>
2021-07-05 17:08:14 -0300

    Botas trocadas

--- botas trocadas/botas_trocadas.py
+++ botas trocadas/botas_trocadas.py
@@ -1,33 +1,18 @@
-botas = []
-contador = 0
-while True:
-    try:
-        n = int(input())
-        if n % 2 != 0 or n > 10**4 or n < 2:
-            break
-        for i in range(n):
-            bota = input()
-            botas.append(bota)
-            numero = int(bota.split()[0])
-            lado = bota.split()[1]
-            if numero < 30 or numero >60:
-                break
-            elif lado != "D" and lado != "E":
-                break
-            elif lado == 'D':
-                if '{} {}'.format(numero, 'E') in botas:
-                    contador+=1
-                    botas.remove('{} {}'.format(numero, 'E'))
-                    botas.remove('{} {}'.format(numero, 'D'))
-                if i == n - 1:
-                    print(contador)
-            elif lado == 'E':
-                if '{} {}'.format(numero, 'D') in botas:
-                    contador += 1
-                    botas.remove('{} {}'.format(numero, 'E'))
-                    botas.remove('{} {}'.format(numero, 'D'))
-                if i == n - 1:
-                    print(contador)
-        break
-    except:
-        break
+D = [0] * 31
+E = [0] * 31
+pares = 0
+N = int(input())
+while N > 0:
+    N = N - 1
+    numero, lado = input().split()
+    numero = int(numero)
+    if lado == 'E':
+        E[numero-30] = E[numero-30] + 1
+    else:
+        D[numero-30] = D[numero-30] + 1
+
+    if E[numero-30] and D[numero-30]:
+        E[numero-30] = E[numero-30] - 1
+        D[numero-30] = D[numero-30] - 1
+        pares += 1
+print(pares)
